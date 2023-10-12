2e323351d1859c81895fafe2af120ee3a40422ee
JacobLind <jbl@inspari.dk>
2023-10-12 11:08:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-streamlit.stop()
+#streamlit.stop()
 
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
